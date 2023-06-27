aadab7b1525829365f16f6520ac5e3a5f63c4e0e
Neftaly <neftalyrv23@gmail.com>
2023-06-27 18:56:07 -0400

    Logic for the homepage

---
+++ src/homeFactory.js
@@ -0,0 +1,28 @@
+import backgroundImage from './images/home-background.jpg';
+import Utils from "./Utils";
+
+export default function homeFactory() {
+
+    const mainElement = Utils.createComponent('main');
+    Utils.addStyle(mainElement, 'home');
+    const content = Utils.createComponent('div');
+    Utils.addStyle(content, 'content');
+
+    const contentTitle = Utils.createComponent('h1');
+    contentTitle.textContent = 'Grand Opening!';
+
+    // const card = Utils.createComponent('div');
+    // Utils.addStyle(card, 'card');
+    // Utils.addStyle(card, 'border');
+    // Utils.addStyle(card, 'box-shadow');
+
+    content.appendChild(contentTitle);
+    // content.appendChild(card);
+
+    mainElement.appendChild(content);
+    Utils.addImage(mainElement, backgroundImage);
+
+    return {
+        mainElement
+    }
+}
