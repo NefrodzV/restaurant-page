d35460fca25efaa22f70f9d5f3d2774a05040a53
Neftaly <neftalyrv23@gmail.com>
2023-06-27 18:55:30 -0400

    Deleted the function that are used by the Utils class and change the logic of the main page component to be created by a facotry function

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
 import './style.css';
-import navMenu from './NavMenu';
 import Utils from './Utils';
 import backgroundImage from './images/home-background.jpg';
-import Header from './Header.js';
-import Footer from './Footer.js';
+import Header from './headerFactory.js';
+import Footer from './footerFactory.js';
+import homeFactory from './homeFactory';
 
-const navigation = navMenu;
 
-function createComponent(type) {
-    const element = document.createElement(type);
-    console.log("Created a " + type);
-    return element;
-}
-
-function navComponent() {
-    const nav = Utils.createComponent('nav');
-    const items = navigation.navItems;
-
-    for (let i = 0; i < items.length; i++) {
-        let navItem = items[i];
-        let anchor = Utils.createComponent('a');
-        addStyle(anchor, 'navItem')
-        anchor.textContent = navItem.title;
-        anchor.href = `#`;
-        nav.appendChild(anchor);
-    }
-
-    return nav;
-}
-
-function addStyle(element, style) {
-    element.classList.add(style)
-}
-
-function addImage(element, source) {
-    const image = new Image();
-    image.src = source;
-    element.appendChild(image);
-}
 
 // Main Page Components
 
@@ -45,27 +13,7 @@ function addImage(element, source) {
 const header = Header().headerElement;
 
 // Main
-const main = Utils.createComponent('main');
-
-const content = Utils.createComponent('div');
-addStyle(content, 'content');
-
-
-const contentTitle = createComponent('h1');
-contentTitle.textContent = "Nana's Kitchen!";
-
-const card = Utils.createComponent('div');
-addStyle(card, 'card');
-addStyle(card, 'border');
-addStyle(card, 'box-shadow');
-
-content.appendChild(contentTitle);
-content.appendChild(card);
-
-main.appendChild(content);
-addImage(main, backgroundImage)
-
-
+const main = homeFactory().mainElement;
 
 // Footer
 const footer = Footer().footerElement;
