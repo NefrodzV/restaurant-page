e215803a7518c27c539e83e6088bfd25b188ccbe
Neftaly <neftalyrv23@gmail.com>
2024-10-29 14:52:14 -0400

    Made the logic to the hamburger menu and its styling with its transitions

---
+++ src/components/HamburgerButton.js
@@ -0,0 +1,39 @@
+import { createElement } from "../utils.js";
+
+export default function HamburgerButton( attributes , onClick) {
+    const button = createElement('button', 'hamburger', null, {
+        'data-open': 'false',
+        'aria-expanded': 'false',
+        'aria-label': 'Open navigation'
+    })
+
+    if(attributes) {
+        for(const [key, val] of Object.entries(attributes)) {
+            button.setAttribute(key, val)
+        }
+    }
+
+    // Setting lines for hamburger
+    for(let i = 0; i < 3; i++) {
+        const line = createElement('div')
+        button.appendChild(line)
+    }
+
+    button.addEventListener('click', () => {
+        console.log('click button')
+        const isOpen = button.getAttribute('data-open') === 'true'
+        console.log(isOpen)
+
+        if(isOpen) {
+            button.setAttribute('data-open', 'false');
+            button.setAttribute('aria-expanded', 'false')
+            button.setAttribute('aria-label', 'Open navigation')
+        } else {
+            button.setAttribute('data-open', 'true');
+            button.setAttribute('aria-expanded', 'true')
+            button.setAttribute('aria-label', 'Close navigation')
+        }
+    })
+
+    return button
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import { createElement } from "../utils.js";
+import HamburgerButton from "./HamburgerButton.js";
 import  Navigation  from "./Navigation.js";
 
 export default function Header(onNavItemClick) {
     const header = createElement('header')
     const logo = createElement('div', 'logo')
     logo.textContent = 'Logo section'
-
     const navigation = Navigation(onNavItemClick)
-    header.append(logo, navigation)
+    const hamburgerButton = HamburgerButton()
+    header.append(logo, hamburgerButton, navigation)
+
     return header
 }
