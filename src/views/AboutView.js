1f62748bc4ddb46a6c57beaaadeb11a84c149eb1
Neftaly <neftalyrv23@gmail.com>
2024-11-13 22:08:19 -0400

    changed the name of the article

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -6,14 +6,17 @@ export default function AboutView() {
     const title = createElement('h1')
     title.textContent = 'About Fusion Flavor'
 
-    const article = createElement('article', 'details')
+    const aboutUsArticle = createElement('article', 'details')
     const logo = FusionFlavorLogo()
     const aboutUsDiv = createElement('div')
     const aboutUsTitle = createElement('h2', 'title')
     aboutUsTitle.textContent = 'About Us'
     const aboutUsParagraph = "At Fusion Flavor, we’re more than just a restaurant – we’re a culinary destination that brings together the best of global cuisines with a creative twist. Our philosophy is simple: great food tells a story, and we want every dish to transport you to a new place and time. Our team of chefs is dedicated to pushing the boundaries of traditional cooking, blending flavors, textures, and techniques from different cultures to craft an experience that’s as exciting as it is delicious. Whether it’s an unexpected pairing or a fresh take on a classic favorite, Fusion Flavor is all about reimagining the way we experience food. We believe that every meal should be an adventure. From the bold spices of Southeast Asia to the comforting flavors of Mediterranean cuisine, our menu is a vibrant fusion of the world’s most beloved culinary traditions. We’re committed to using only the freshest, locally-sourced ingredients to bring these global flavors to life."
     aboutUsDiv.append(aboutUsTitle, aboutUsParagraph)
-    article.append(aboutUsDiv, logo)
-    section.append(title, article)
+    aboutUsArticle.append(aboutUsDiv, logo)
+
+    // Working hours article
+
+    section.append(title, aboutUsArticle)
     return section
 }
